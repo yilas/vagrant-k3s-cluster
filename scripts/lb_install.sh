444e5197d80bd55f1b24a4dcf07471ead03791bb
Yann ILAS <yann.ilas@gmail.com>
2023-06-21 16:54:29 +0200

    Update version

--- scripts/lb_install.sh
+++ scripts/lb_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Download and deploy Traefik as a front load balancer
-curl https://github.com/traefik/traefik/releases/download/v2.9.5/traefik_v2.9.5_linux_amd64.tar.gz -o /tmp/traefik.tar.gz -L
+curl https://github.com/traefik/traefik/releases/download/v2.10.3/traefik_v2.10.3_linux_amd64.tar.gz -o /tmp/traefik.tar.gz -L
 cd /tmp/
 tar xvfz ./traefik.tar.gz
-nohup ./traefik --configFile=/tmp/traefikconf/static_conf.toml &> /dev/null&
+rm -f ./traefik.tar.gz
+nohup ./traefik --configFile=/tmp/traefikconf/static_conf.toml &> /dev/null &
